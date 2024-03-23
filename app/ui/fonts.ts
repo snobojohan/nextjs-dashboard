e4eceb0c27e82f8c9270f5382af298f68211ff71
Johan Hallberg <johan.hallberg@svt.se>
2024-03-23 19:45:20 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+
+import { Asap, Lusitana } from 'next/font/google';
+ 
+export const asap = Asap({ subsets: ['latin-ext'] });
+export const lusitana = Lusitana({ 
+  weight: ['400','700'], 
+  subsets: ['latin'] 
+});
